533cb5806fa5490c25c3bfb83ae809e8cee603e3
paulodsn <pauloricardodsn@gmail.com>
2020-03-28 14:28:08 -0300

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,13 @@
+<script>
+    import Layout from './app/layout.svelte';
+</script>
+
+<Layout>
+    <h1>Ano Hana</h1>
+</Layout>
+
+<style type="text/postcss">
+    h1 {
+        @apply text-xl font-semibold mb-4;
+    }
+</style>
---
+++ src/app/layout.svelte
@@ -0,0 +1,9 @@
+<main>
+    <slot></slot>
+</main>
+
+<style type="text/postcss">
+    main {
+        @apply bg-white mx-4 my-2 p-4;
+    }
+</style>
