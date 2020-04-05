805e685f908b9ad70c24120bfb6cb6c30fa8c405
paulodsn <pauloricardodsn@gmail.com>
2020-04-05 03:13:19 -0300

    chore: add smooth scroll lib

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import './global.css';
 import App from './App.svelte';
+import smoothScroll from 'smooth-scroll';
+
+new smoothScroll('a[href*="#"]');
 
 const app = new App({
   target: document.body,
